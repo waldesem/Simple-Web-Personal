4d7290baaf8f61481cdc712d2fd5d7bda38bb763
Vladimir Semenenko <wsemenenko@gmail.com>
2026-03-01 09:15:17 +0300

    Update build

---
+++ server_flask/app/static/assets/ResumeForm-CUzeuRic.js
@@ -0,0 +1,1 @@
+import{c as m}from"./ResumeForm.vue_vue_type_script_setup_true_lang-DDY5cRKa.js";import"./index-B7pML0Xn.js";export{m as default};
--- server_flask/app/static/assets/ResumeForm-CgEzp1PF.js
+++
@@ -1,1 +0,0 @@
-import{c as m}from"./ResumeForm.vue_vue_type_script_setup_true_lang-Cb1XuC_A.js";import"./index-DqR8deQR.js";export{m as default};
