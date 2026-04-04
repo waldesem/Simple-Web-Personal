80de688e297d7ccdc53c4645188307d756d30b37
Vladimir Semenenko <wsemenenko@gmail.com>
2026-04-04 09:41:32 +0300

    Update vesions

---
+++ server_flask/app/static/assets/ResumeForm-CA5rkwth.js
@@ -0,0 +1,1 @@
+import{c as m}from"./index-Dxh0aTx9.js";import"./index-CBSZRror.js";export{m as default};
--- server_flask/app/static/assets/ResumeForm-DjPcmEOz.js
+++
@@ -1,1 +0,0 @@
-import{c as m}from"./ResumeForm.vue_vue_type_script_setup_true_lang-Cb_VpPhf.js";import"./index-BhSCS-Rz.js";export{m as default};
